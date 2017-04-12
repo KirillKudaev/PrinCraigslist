bf03871c8bcb2f211e9c79394f952d64b549fb7f
KirillKudaev <kirill.a.kudaev@gmail.com>
2017-04-12 11:35:27 -0500

    Added iOS Unit Tests

---
+++ iOS/ParseStarterProject/Item.swift
@@ -0,0 +1,9 @@
+//
+//  Item.swift
+//  PrinCraigslist
+//
+//  Created by Kirill Kudaev on 4/12/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import Foundation
--- iOS/PrinCraigslistTests/PrinCraigslistTests.swift
+++ iOS/PrinCraigslistTests/PrinCraigslistTests.swift
@@ -6,14 +6,25 @@
 //  Copyright © 2017 Parse. All rights reserved.
 //
 
+import UIKit
 import XCTest
 
+@testable import PrinCraigslist
+
 class PrinCraigslistTests: XCTestCase {
-    
+    var systemUnderTest: ViewController!
+
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
+        
+        //get the storyboard the ViewController under test is inside
+        let storyboard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        
+        //get the ViewController we want to test from the storyboard (note the identifier is the id explicitly set in the identity inspector)
+        systemUnderTest = storyboard.instantiateViewController(withIdentifier: "MyViewController") as! ViewController
+        
+        //load view hierarchy
+        _ = systemUnderTest.view    }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
@@ -30,6 +41,46 @@ class PrinCraigslistTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    func testSUT_TableViewIsNotNilAfterViewDidLoad() {
+        
+        XCTAssertNotNil(systemUnderTest.tableView)
+    }
+    
+    func testSUT_ShouldSetTableViewDataSource() {
+        
+        XCTAssertNotNil(systemUnderTest.tableView.dataSource)
+    }
+    
+    func testSUT_ShouldSetTableViewDelegate() {
+        
+        XCTAssertNotNil(systemUnderTest.tableView.dataSource)
+    }
+    
+    func testSUT_ConformsToTableViewDataSourceProtocol() {
+        
+        XCTAssert(systemUnderTest.conformsToProtocol(UITableViewDataSource))
+        
+        XCTAssert(systemUnderTest.respondsToSelector(#selector(systemUnderTest.numberOfSectionsInTableView(_:))))
+        
+        XCTAssert(systemUnderTest.respondsToSelector(#selector(systemUnderTest.tableView(_:numberOfRowsInSection:))))
+        
+        XCTAssert(systemUnderTest.respondsToSelector(#selector(systemUnderTest.tableView(_:cellForRowAtIndexPath:))))
+    }
+    
+    func testSUT_TableViewUsesCustomCell_SearchItemTableViewCell() {
+        
+        let firstCell = systemUnderTest.tableView(systemUnderTest.tableView, cellForRowAtIndexPath: NSIndexPath(forRow: 0, inSection: 0))
+        
+        XCTAssert(firstCell is SearchItemTableViewCell) // change the name of the cell
+    }
+    
+    func testSUT_ConformsToTableViewDelegateProtocol() {
+        
+        XCTAssert(systemUnderTest.conformsToProtocol(UITableViewDelegate))
+        
+        XCTAssert(systemUnderTest.respondsToSelector(#selector(systemUnderTest.tableView(_:didSelectRowAtIndexPath:))))
     }
     
 }
