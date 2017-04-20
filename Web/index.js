e8cd7ecf4603c55b3d83a247cf95dd9f7c4c949d
ggoodsell694 <gavin.goodsell@principia.edu>
2017-04-20 01:56:09 -0500

    JS unit tests for Validator mostly done

--- Web/index.js
+++ Web/index.js
@@ -10,12 +10,24 @@ function signupUser() {
 	event.preventDefault();
 
 	var driver = new Driver();
-
+	console.log("about to call driver");
 	//Need to wait for 
-	var resp = driver.signUpUser($("#createFirstName").val(), $("#createLastName").val(),
+	var errors = driver.signUpUser($("#createFirstName").val().trim(), $("#createLastName").val(),
 	 $("#createEmail").val(), $("#createUserPass").val());
+		
+	console.log("before validation");
+	//Validation
+	if (errors.length > 0) {
+		console.log("there were errors" + errors + "lkasdf");
+		errors.forEach(function(err) {
+			console.log("error");
+			alert("Error - " + err.tag + " : " + err.desc);
+		});
+	}
+	else 
+		console.log("no errors");
+		
 	
-	//add response validation
 }
 
 function getUser() {
@@ -26,10 +38,14 @@ function getUser() {
 	//Need to wait for 
 	var user = driver.getUser();
 	
+	
 	console.log("email: " + user.email);
 	console.log("first: " + user.firstName);
 	console.log("last: " + user.lastName);
 		
+	
+	
+	
 		
 }
 
@@ -39,14 +55,20 @@ function updateUser() {
 	var driver = new Driver();
 
 	//Need to wait for 
-	var user = driver.updateUser($("#updateFirstName").val(), $("#updateLastName").val(),
-	 $("#updateEmail").val(), $("#updateUserPass").val());
+	var errors = driver.updateUser($("#updateFirstName").val(), $("#updateLastName").val(),
+	 $("#updateEmail").val(), $("#updateUserPass").val(), $("#updateUserOldPass").val());
+	 
+	//var user = driver.updateUser("update", "update", "update@u", "updateme", "somethingelse");
 	
-	console.log("email: " + user.get("email"));
-	console.log("first: " + user.get("firstName"));
-	console.log("last: " + user.get("lastName"));
-	
-	//add response validation
+	if (errors.length > 0) {
+		console.log(errors);
+		errors.forEach(function(err) {
+			console.log("error");
+			alert("Error - " + err.tag + " : " + err.desc);
+		});
+	}
+	else 
+		console.log("no errors");
 			
 }
 
@@ -57,6 +79,14 @@ function deleteUser() {
 	
 	driver.deleteUser();
 }
+
+function logoutUser() {
+	event.preventDefault();
+	
+	var driver = new Driver();
+	
+	driver.logoutUser();
+}
 //################################################################################################
 
 
@@ -64,7 +94,7 @@ function deleteUser() {
 //Model 
 //************************************************************************************************
 function Driver() {
-	//this.user = new Parse.User();
+	this.vld = new Validator();
 }
 
 
@@ -81,7 +111,15 @@ Driver.prototype = {
 		console.log("last: " + lastName);
 		console.log("pass: " + password);
 		
+		var vld = new Validator();
+		var errors = vld.validateCreateUser(firstName, lastName, email, password);
+		
+		if (errors.length > 0)
+			return errors;
+
 		var user = new Parse.User();
+		
+		
 		
 		//set values for user
 		user.set("firstName", firstName);
@@ -102,6 +140,8 @@ Driver.prototype = {
 				alert("Error: " + error.code + " " + error.message);
 		  }
 		});
+		
+		return [];
 	},
 	
 	
@@ -115,6 +155,8 @@ Driver.prototype = {
 		//var user = new User (userTemp.get("firstName"), 
 		//userTemp.get("lastName"), userTemp.get("email"));
 		
+		
+		
 		return {
 			"firstName" : user.get("firstName"),
 			"lastName"  : user.get("lastName"),
@@ -127,6 +169,14 @@ Driver.prototype = {
 	*	RETURN: Array of validator tags indicating errors, or empty array if none 
 	*/
 	updateUser : function(firstName, lastName, email, password, oldPassword) {
+		
+		var errors = []
+		var vld = new Validator();
+		
+		errors = vld.validateUpdateUser(firstName, lastName, email, password, oldPassword)
+		
+		if (errors.length > 0)
+			return errors;
 		
 		//Get current user
 		var user = new Parse.User.current();
@@ -143,7 +193,7 @@ Driver.prototype = {
 		if (password)
 			user.set("password", password);
 		
-		return user;	
+		return [];	
 	},
 	
 	/*
@@ -169,12 +219,258 @@ Driver.prototype = {
 		});
 		
 		return user;	
+	},
+	
+	logoutUser : function() {
+		
+		Parse.User.logOut();
+		
+		
+		
 	}
+	
+	
 	
 };
 
 
+function Validator() {
+	//Character length constraints 
+	this.titleMax = 80; 
+	this.descriptionMax = 1000; 
+	this.firstNameMax = 35;
+	this.lastNameMax = 35;
+	this.emailMax = 70;
+	this.passwordMax = 35;
+	this.passwordMin = 8;
+	
+	//field/value constraints
+	this.validPicFormats = ["jpg"]; //Currently only accepting jpeg
+	this.validCategories = ["Books", "Bikes", "Electronics", "Clothing", "Jobs", "Other"];
+	//this.requiredItemFields = ["userId", "title", "categoryId", "price", "postedDate"];
+	//this.requiredUserFields = ["lastName", "email", "password"];
+	//this.errors;
+	
+	this.tags = {
+		missingField: "Missing Field",         // Field missing from request. 
+		badValue: "Bad Value",                 // Field has bad value.  
+		notFound: "Not Found",                 // Entity not present in DB
+		badLogin: "Bad Login",                 // Email/password combination invalid
+		forbiddenField: "forbiddenField",      // Field not alowed to be altered
+		noOldPwd: "No Old Password",           // Change of password requires an old password
+		oldPwdMismatch: "Old Password Failed", // Old Password doesn't match
+		badCategory:"Bad Category"             // Category is not found in list
+	};
+	
+	//
+	this.desc = {
+		firstNameMax   : "First name must be less than " + this.firstNameMax + " characters.", 
+		lastNameMax    : "Last name must be less than " + this.lastNameMax + " characters.",
+		emailMax       : "Email must be less than " + this.emailMax + " characters.",
+		passwordMax    : "Last name must be less than " + this.lastNameMax + " characters.",
+		passwordMin    : "Password must be at least " + this.passwordMin + " characters.",
+		titleMax       : "Title must be less than " + this.titleMax + " characters.",
+		descriptionMax : "Description must be less than " + this.descriptionMax + " characters.",
+		vldCategory    : "Valid categories are: Books, Bikes, Electronics, Clothing, Jobs, or Other",
+		vldPicFormat   : "Valid picture format is only .jpg at this time",
+		oldPassFail    : "Password validation failed."
+	};
+}
 
+
+Validator.prototype = {
+	
+	//########################################################
+	//PUBLIC METHODS
+	//########################################################
+	validateCreateUser : function(firstName, lastName, email, password) {
+		var errors = [];
+
+		errors = errors.concat(this.validateUserFieldLengths(firstName, lastName, email, password));
+		
+		errors = errors.concat(this.validateUserRequiredFields(lastName, email));
+		
+		return errors;
+	},
+	
+	validateUpdateUser : function(firstName, lastName, email, password, oldPassword) {
+		
+		var errors = [];
+		
+		errors = errors.concat(this.validateUserFieldLengths(firstName, lastName, email, password));
+		
+		//errors = errors.concat(this.validateUserRequiredFields(lastName, email));
+		
+		//errors = errors.concat(this.validateUserOldPassword(oldPassword));
+		
+		return errors;
+	},
+	
+	validateCreateItem : function(title, description, picture, category, price) {
+		var errors = [];
+		
+		errors = errors.concat(this.validateItemFieldLengths(title, description));
+		
+		errors = errors.concat(this.validateItemPicture(picture));
+		
+		errors = errors.concat(this.validateItemCategories(picture));
+		
+		errors = errors.concat(this.validateItemPrice(price));
+		
+		
+		return errors;
+
+		
+	},
+	
+	validateUpdateItem : function(title, description, picture, category, price) {
+		var errors = [];
+		
+		return errors;
+	},
+	
+	
+	
+	//########################################################
+	
+	
+	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	//PRIVATE METHODS
+	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+	validateUserFieldLengths : function (firstName, lastName, email, password) {
+		var errors = [];
+		
+		if (firstName && firstName.length > 0 && firstName.length > this.firstNameMax)
+			
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : this.desc.firstNameMax
+			});
+		
+		
+		if (lastName && lastName.length > 0 && lastName.length > this.lastNameMax)
+			
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : this.desc.lastNameMax
+			});
+		
+		if (email && email.length > 0 && email.length > this.emailMax)
+			
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : this.desc.emailMax
+			});
+		
+		if (password && password.length > 0 && password.length > this.passwordMax)
+			
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : this.desc.passwordMax 
+			});
+		
+		if (password && password.length > 0 && password.length < this.passwordMin)
+			
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : this.desc.passwordMin 
+			});
+		
+		
+		
+		return errors;
+	},
+	
+	validateUserRequiredFields : function(lastName, email) {
+		
+		var errors = [];
+		
+		/*
+		if (lastName && lastName.length > 0 && lastName.trim().length < 1 || email.trim().length < 1)
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : "Last name and email must have at least 1 non-whitespace character."
+			});
+		
+		
+		if (email && email.length > 0 && email.indexOf('@') > -1)
+			errors.push({
+				"tag" : this.tags.badValue, 
+				"desc" : email + " is not a valid email."
+			});
+			
+		*/	
+		
+		return errors;
+	},
+	
+	validateUserOldPassword : function (oldPassword) {
+		var errors = [];
+		var user = Parse.User.current();
+		var temp = [];
+		var query = new Parse.Query(user);
+		
+		query.equalTo("password", oldPassword);
+		temp.push(query);
+		
+		temp.forEach(function (q) {
+			q.find({
+			    success: function(results) {
+				
+			    },
+			    error: function(error) {
+				   errors.push({
+					   "tag" : this.tags.oldPwdFailed, 
+					   "desc" : this.desc.oldPassFail
+				   });
+			  }
+			});
+		});
+		
+	},
+	
+	
+	validateItemFieldLengths : function (title, description) {
+		var errors = [];
+		
+		
+		return errors;
+	},
+	
+	validateItemPicture : function (picture) {
+		var errors = [];
+		
+		
+		return errors;
+	},
+	
+	
+		
+	validateItemCategories : function (picture) {
+		var errors = [];
+		
+		
+		return errors;
+	},
+	
+	
+		
+	validateItemPrice : function(price) {
+		var errors = [];
+		
+		
+		return errors;
+	},
+	
+	validateItemRequiredFields : function(price) {
+		var errors = [];
+		
+		
+		return errors;
+	}
+	
+	//$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+};
 
 
 
