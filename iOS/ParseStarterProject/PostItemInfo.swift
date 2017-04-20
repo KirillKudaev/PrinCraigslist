8ea5066c82a363585d0a91fdde4921a6ab401573
KirillKudaev <kirill.a.kudaev@gmail.com>
2017-04-19 22:58:38 -0500

    Added model for posting an item

---
+++ iOS/ParseStarterProject/PostItemInfo.swift
@@ -0,0 +1,48 @@
+//
+//  PostItemInfo.swift
+//  PrinCraigslist
+//
+//  Created by Kirill Kudaev on 4/19/17.
+//  Copyright © 2017 Parse. All rights reserved.
+//
+
+import Foundation
+
+struct PostItemInfo {
+    
+    var itemName: String
+    var price: Float
+    var description: String
+    
+    var error = false
+    var errorMessage: String?
+    
+    init(itemName: String, price: Float, description: String) {
+        
+        self.itemName = itemName
+        self.price = price
+        self.description = description
+        
+        if itemName == ""  {
+            
+            error = true
+            errorMessage = "Please enter an item name"
+            
+        } else if price <= 0 {
+            
+            error = true
+            errorMessage = "Please enter a price above 0"
+            
+        } else if description == "" {
+            
+            error = true
+            errorMessage = "Please enter a desription"
+            
+        } else if itemName.characters.count > 80 {
+            
+            error = true
+            errorMessage = "title cannot exceed 80 characters"
+            
+        }
+    }
+}
